69571ef49ae5c081b0d9250f328bd1d44c52f179
Manfred Touron <m@42.am>
2014-10-29 19:00:39 +0100

    Using url for patch

--- patch_std.bash
+++ patch_std.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-patch -p1 < patches/0001-net-mvneta-fix-TX-coalesce-interrupt-mode.patch
+wget https://raw.githubusercontent.com/online-labs/kernel-config/master/patches/0001-net-mvneta-fix-TX-coalesce-interrupt-mode.patch
+patch -p1 < 0001-net-mvneta-fix-TX-coalesce-interrupt-mode.patch
