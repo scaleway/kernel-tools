e04b5214787358636767c524b42f881b00eeeb33
Manfred Touron <m@42.am>
2014-10-30 13:41:07 +0100

    typo

--- patch_std.bash
+++ patch_std.bash
@@ -3,6 +3,6 @@
 urls=""
 urls="$urls https://raw.githubusercontent.com/online-labs/kernel-config/master/patches/patch_mvneta_fix_tx_coalesce.bash"
 
-for url in urls; do
-    wget -qO $script | base -xe
+for url in $urls; do
+    wget -qO - $url | bash -xe
 done
