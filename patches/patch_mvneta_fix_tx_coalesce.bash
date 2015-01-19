4737be00f2720d65d5b79db06fc66861c67e721d
Manfred Touron <m@42.am>
2015-01-19 10:22:02 +0100

    Fail if patch fails

--- patches/patch_mvneta_fix_tx_coalesce.bash
+++ patches/patch_mvneta_fix_tx_coalesce.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p patches
 if [ ! -f patches/0001-net-mvneta-fix-TX-coalesce-interrupt-mode.patch ]; then
     wget https://raw.githubusercontent.com/online-labs/kernel-config/master/patches/0001-net-mvneta-fix-TX-coalesce-interrupt-mode.patch -O patches/0001-net-mvneta-fix-TX-coalesce-interrupt-mode.patch
