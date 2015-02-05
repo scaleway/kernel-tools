955eaa2cf3e91333607d281bfe6cbb5fae9a6e93
Sebastien Rannou <srannou@ocs.online.net>
2015-02-05 16:40:06 +0100

    Fixed patching of kernek 3.2.34-aufs (2).

--- 3.2.34-aufs/patch.sh
+++ 3.2.34-aufs/patch.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-for file in patches/aufs-aufs3-debian-3.2/*
-do
-    patch -p1 < $file
-done
-
+patch -p1 < patches/aufs-aufs3-debian-3.2/aufs3-add.patch
+patch -p1 < patches/aufs-aufs3-debian-3.2/aufs3-base.patch
+patch -p1 < patches/aufs-aufs3-debian-3.2/aufs3-fix-export-__devcgroup_inode_permission.patch
+patch -p1 < patches/aufs-aufs3-debian-3.2/aufs3-kbuild.patch
+patch -p1 < patches/aufs-aufs3-debian-3.2/aufs3-standalone.patch
+patch -p1 < patches/aufs-aufs3-debian-3.2/mark-as-staging.patch
