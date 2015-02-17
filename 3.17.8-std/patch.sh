2973d7d0eb56135afc6ff84814d63c910d0ab796
Sebastien Rannou <srannou@ocs.online.net>
2015-02-17 14:12:58 +0100

    Fixed patches in kernel 3.17.8-std.

--- 3.17.8-std/patch.sh
+++ 3.17.8-std/patch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-patch -p1 < patches/0001-enable-hardware-I-O-coherency.patch
-patch -p1 < patches/patch-cpuidle.patch
+patch -p1 < patches/patch-disable-IO-coherency-3.17-std.patch
+patch -p1 < patches/patch-cpuidle-3.17-std.patch
