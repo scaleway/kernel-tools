b9392ffcc0c550b4294aab5848607f00331126d7
Sebastien Rannou <srannou@ocs.online.net>
2015-03-04 18:38:09 +0100

    Enable hardware IO coherency on 3.17.8-std.

--- 3.17.8-std/patch.sh
+++ 3.17.8-std/patch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-patch -p1 < patches/patch-disable-IO-coherency-3.17-std.patch
+patch -p1 < patches/patch-enable-IO-coherency-3.17-std.patch
 patch -p1 < patches/patch-cpuidle-3.17-std.patch
