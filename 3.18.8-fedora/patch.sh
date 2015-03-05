d0cb461c9743fb6a98f17db2f7e1aa97b714dbc9
Sebastien Rannou <srannou@ocs.online.net>
2015-03-05 09:25:09 +0100

    Enable hardware IO coherency on 3.18.8-fedora.

--- 3.18.8-fedora/patch.sh
+++ 3.18.8-fedora/patch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 patch -p1 < patches/patch-cpuidle-3.17-std.patch
+patch -p1 < patches/0001-enable-hardware-I-O-coherency.patch
 patch -p1 < patches/0001-ntp-Fixup-adjtimex-freq-validation-on-32bit-systems.patch
