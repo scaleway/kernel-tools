07ead4bc33cd23704512babe00fe98db60bf1c72
Sebastien Rannou <srannou@ocs.online.net>
2015-03-05 09:56:18 +0100

    Added cpuidle and IO coherency patch on 4.0 kernels. Fixes #44

--- 4.0-rc1-fedora/patch.sh
+++ 4.0-rc1-fedora/patch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-#patch -p1 < patches/patch-cpuidle-3.17-std.patch
+patch -p1 < patches/patch-enable-IO-coherency-4.0.patch
+patch -p1 < patches/patch-cpuidle-4.0.patch
 #patch -p1 < patches/0001-ntp-Fixup-adjtimex-freq-validation-on-32bit-systems.patch
---
+++ 4.0-rc1-std/patch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+patch -p1 < patches/patch-enable-IO-coherency-4.0.patch
+patch -p1 < patches/patch-cpuidle-4.0.patch
