7b168e78d1c3584a9174d50abee998af47bfeb53
Manfred Touron <m@42.am>
2015-03-12 16:25:58 +0100

    Bumped 3.18.9-aufs to 3.19.1-aufs

--- 3.19.1-aufs/patch.sh
+++ 3.19.1-aufs/patch.sh
@@ -2,4 +2,4 @@
 
 /bin/bash -x patches/patch_aufs.bash
 patch -p1 < patches/0001-enable-hardware-I-O-coherency.patch
-patch -p1 < patches/patch-cpuidle-3.17-std.patch
+patch -p1 < patches/patch-cpuidle-3.19-std.patch
