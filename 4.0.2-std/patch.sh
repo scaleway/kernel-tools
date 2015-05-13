39c9faf77d7bbf10d8f45c02a48d30a72a565587
Manfred Touron <m@42.am>
2015-05-13 12:26:16 +0200

    Make git repo clean after patches

--- 4.0.2-std/patch.sh
+++ 4.0.2-std/patch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 patch -p1 < patches/patch-enable-IO-coherency-4.0.patch
+git update-index --assume-unchanged arch/arm/mach-mvebu/coherency.c
 #patch -p1 < patches/patch-cpuidle-4.0.patch
