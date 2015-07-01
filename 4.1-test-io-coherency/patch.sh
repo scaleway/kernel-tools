776a4838d1240240a07784f6393398590f8354c8
Sebastien Rannou <srannou@ocs.online.net>
2015-07-01 18:26:56 +0200

    Fix mvneta regression on 4.1 kernels (thanks to @tpetazzoni).

--- 4.1-test-io-coherency/patch.sh
+++ 4.1-test-io-coherency/patch.sh
@@ -3,3 +3,4 @@
 #patch -p1 < patches/patch-enable-IO-coherency-4.0.patch
 #git update-index --assume-unchanged arch/arm/mach-mvebu/coherency.c
 #patch -p1 < patches/patch-cpuidle-4.0.patch
+patch -p1 < patches/patch-mvneta-fix-init-nego.patch
