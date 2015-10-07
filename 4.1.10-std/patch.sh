3412d795c253799b0e9ce7bcea4dc5f56bf7d773
Quentin Perez <qperez@ocs.online.net>
2015-10-07 10:28:41 +0200

    bump 4.1.10

--- 4.1.10-std/patch.sh
+++ 4.1.10-std/patch.sh
@@ -5,7 +5,7 @@ git update-index --assume-unchanged arch/arm/mach-mvebu/coherency.c
 #patch -p1 < patches/patch-cpuidle-4.0.patch
 
 # sgmii mvneta
-patch -p1 < patches/patch-inband-status_1.patch
-patch -p1 < patches/patch-inband-status_2.patch
-patch -p1 < patches/patch-inband-status_3.patch
-patch -p1 < patches/patch-inband-status_4.patch
+#patch -p1 < patches/patch-inband-status_1.patch
+#patch -p1 < patches/patch-inband-status_2.patch
+#patch -p1 < patches/patch-inband-status_3.patch
+#patch -p1 < patches/patch-inband-status_4.patch
