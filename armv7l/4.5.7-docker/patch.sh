cfde8e2cc672f4e34ae6938c4c21e22f51ac1c9a
Quentin Perez <qperez@ocs.online.net>
2016-09-28 16:55:08 +0200

    armv7l revert bump 4.7.3

--- armv7l/4.5.7-docker/patch.sh
+++ armv7l/4.5.7-docker/patch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-KVER=4.7 /bin/bash -x patches/patch_aufs.bash
+KVER=4.5 /bin/bash -x patches/patch_aufs.bash
 
 patch -p1 < patches/patch-enable-IO-coherency-4.0.patch
 git update-index --assume-unchanged arch/arm/mach-mvebu/coherency.c
